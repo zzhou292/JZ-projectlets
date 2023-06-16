99647f354d21115d7206d63048284cf4ee37cc6f
jason <zzhou292@wisc.edu>
2023-06-16 11:24:11 -0500

    added leet0141

---
+++ LeetDaily/0141_linked_list_cycle/list_cycle.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        // edge case
+        if(head == nullptr || head->next == nullptr){
+            return false;
+        }
+
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+        while(fast!=nullptr && fast->next!=nullptr && slow!=nullptr){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow) return true;
+        }
+
+        return false;
+    }
+};
