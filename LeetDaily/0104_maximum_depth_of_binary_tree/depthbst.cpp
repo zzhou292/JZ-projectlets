77ac46bea4cd25c9fe62397c67f353fbf58b6020
jason <zzhou292@wisc.edu>
2023-06-18 22:44:44 -0500

    added leet0104

---
+++ LeetDaily/0104_maximum_depth_of_binary_tree/depthbst.cpp
@@ -0,0 +1,28 @@
+// Author: Jason Zhou
+#include "../general_include.h"
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+  int maxDepth(TreeNode *root) {
+    if (root == nullptr)
+      return 0;
+
+    int left_depth = maxDepth(root->left);
+    int right_depth = maxDepth(root->right);
+    return max(left_depth, right_depth) + 1;
+  }
+};
