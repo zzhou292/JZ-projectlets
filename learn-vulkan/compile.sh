5cbc0261a93855c5b413851b7301b1b28e604363
jason <zzhou292@wisc.edu>
2023-12-27 18:19:09 -0600

    small

---
+++ learn-vulkan/compile.sh
@@ -0,0 +1,3 @@
+g++ -std=c++17 -O2 -o VulkanTest main.cpp -lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi
+
+g++ -std=c++17 -O2 -o triangle triangle.cpp -lglfw -lvulkan -ldl -lpthread -lX11 -lXxf86vm -lXrandr -lXi
